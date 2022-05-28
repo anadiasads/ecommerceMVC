cdbec2a71cb84b10d61bb5a7e2e1c13a0da471c4
Ana Dias <anadias@delage.com.br>
2022-05-28 11:32:59 -0300

    Inserindo a partir do ProdutoRepository

--- Aulas/Aula2/CasaDoCodigo/DataService.cs
+++ Aulas/Aula2/CasaDoCodigo/DataService.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
 
@@ -7,23 +9,28 @@ namespace CasaDoCodigo
     class DataService : IDataService
     {
         private readonly ApplicationContext context;
+        private readonly IProdutoRepository produtoRepository;
 
-        public DataService(ApplicationContext context)
+        public DataService(ApplicationContext context, IProdutoRepository produtoRepository)
         {
             this.context = context;
+            this.produtoRepository = produtoRepository;
         }
 
         public void InicializaDB()
         {
             context.Database.EnsureCreated();
+            List<Livro> livros = GetLivros();
+            produtoRepository.SaveProdutos(livros);
+
+        }
+
+
+        private static List<Livro> GetLivros()
+        {
             var json = File.ReadAllText("livros.json");
             var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
         }
-    }
-    class Livro
-    {
-        public string Codigo { get; set; }
-        public string Nome { get; set; }
-        public decimal Preco { get; set; }
     }
 }
